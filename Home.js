95f485c42c846eadb9db298a1b28424faf1d5a0d
Sateesh Kumbhakar <>
2022-01-30 17:45:33 +0530

    A component is added to the Home js

--- Home.js
+++ Home.js
@@ -1,1 +1,14 @@
-this if (a<) ? this is the  file of js `
+this if (a<) ? this is the  file of js
+
+import React from 'react';
+import notFoundPage from '../img/notFoundPage.jpg'
+ 
+const PageNotFound = () => {
+
+  return <div className='container  '> <h1 className='large my-3'> Opps! Page Not found...</h1>
+  <img src={notFoundPage} alt="" />
+  </div>;
+};
+
+export default PageNotFound;
+`
