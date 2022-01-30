b491036ac07aff7e45e8f5ce89fe09ce575cf5eb
SATEESH KUMBHAKAR <91566141+SateeshKumbhakar@users.noreply.github.com>
2022-01-30 20:12:39 +0530

    Revert "Sk3"

--- Home.js
+++ Home.js
@@ -1,3 +1,16 @@
--this file added only for testing purpose 
+this if (a<) ? this is the  file of js
+
+import React from 'react';
+import notFoundPage from '../img/notFoundPage.jpg'
  
+const PageNotFound = () => {
+
+  return <div className='container  '> <h1 className='large my-3'> Opps! Page Not found...</h1>
+  <img src={notFoundPage} alt="" />
+  </div>;
+};
+
+export default PageNotFound;
+
+export default PageNotFound;
 
